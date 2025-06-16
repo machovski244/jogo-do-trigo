2b1057b70553903b7261249d07a9550cd98a303a
machovski244 <kaua.machovski@escola.pr.gov.br>
2025-06-16 09:43:40 -0300

    jogo do trigo

---
+++ sketch.js
@@ -0,0 +1,55 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+
+
+
+function draw() {
+  background("gray");
+
+
+fill("green")
+  square(200,200,200)
+fill("green")
+square(200,0,500)
+
+
+
+  
+  
+let xjogador11 = 270
+text("🚚",xjogador11,250)
+textSize(40)  
+fill("#F5DEB3")
+square(200,0,200)
+
+let xjogador1 = 350
+let xjogador2 = 290
+let xjogador3 = 230
+let xjogador4 = 290
+let xjogador5 = 110
+let xjogador6 = 10
+let xjogador7 = 110
+let xjogador8 = 10
+let xjogador9 = 10
+let xjogador10 = 90
+
+text("🏭",xjogador5,60)
+text("🌳",xjogador1,380)
+text("🌳",xjogador2,380)
+text("🌳",xjogador3,380)
+text("👨🏿‍🌾",xjogador4,110)
+text("🏦",xjogador6,60) 
+text("🏥",xjogador7,170)   
+text("🏤",xjogador8,170) 
+text("🏟",xjogador9,280)      
+text("🏪",xjogador10,350)
+
+
+  
+
+
+
+}
+
